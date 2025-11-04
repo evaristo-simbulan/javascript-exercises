5b40ab9f5862a5e9de8902b756d7b4fdb01ea27d
evaristo-simbulan <219189860+evaristo-simbulan@users.noreply.github.com>
2025-11-04 15:55:49 +0800

    Add 06_repeatString solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(itemToRepeat, count) {
 
+    if(count < 0) {
+        return "ERROR";
+    }
+
+    let stringToReturn = "";
+    for(let i = 0; i < count; i++) {
+        stringToReturn += itemToRepeat;
+    }
+    return stringToReturn;
 };
 
 // Do not edit below this line
