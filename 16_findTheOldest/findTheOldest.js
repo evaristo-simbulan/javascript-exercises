afd9738e82d0f76f6a022ac5a13e6398bf87e357
evaristo-simbulan <219189860+evaristo-simbulan@users.noreply.github.com>
2025-11-05 19:21:26 +0800

    Add 16_findTheOlder solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    return arr.reduce((current, item) => {
 
+        let itemYOD;
+        let currentYOD;
+        if ("yearOfDeath" in item) {
+            itemYOD = item.yearOfDeath;
+        } else {
+            itemYOD = new Date().getFullYear();
+        }
+
+        if ("yearOfDeath" in current) {
+            currentYOD = current.yearOfDeath;
+        } else {
+            currentYOD = new Date().getFullYear();
+        }
+
+        let itemAge = itemYOD - item.yearOfBirth;
+        let currentAge = currentYOD - current.yearOfBirth;
+
+        if (itemAge > currentAge) {
+            return item;
+        }
+        return current;
+    });
 };
 
 // Do not edit below this line
