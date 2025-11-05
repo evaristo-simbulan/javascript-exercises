7b794b04d8507273014490efb0185cefee5e546c
evaristo-simbulan <219189860+evaristo-simbulan@users.noreply.github.com>
2025-11-05 18:58:14 +0800

    Add 14_fibonacci solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
 
+    const inputInt = +input;
+
+    if (inputInt < 0) {
+        return "OOPS";
+    }
+    
+    if (inputInt == 0) {
+        return 0;
+    }
+
+    if (inputInt === 1) {
+        return 1;
+    }
+
+    if (inputInt === 2) {
+        return 1;
+    }
+
+    return fibonacci(inputInt - 1) + fibonacci(inputInt - 2);
 };
 
 // Do not edit below this line
