900cdd51d23aa9e9b4ac52f1148f2b37a4d2ba1a
evaristo-simbulan <219189860+evaristo-simbulan@users.noreply.github.com>
2025-11-05 18:44:35 +0800

    Add 13_palindrome solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const clean = input
+        .toLowerCase()
+        .split('')
+        .filter(item => alphanumerical.includes(item))
+        .join('');
+
+    const reversedString = clean.split('').reverse().join('');
+
+    return reversedString === clean;
 };
 
 // Do not edit below this line
