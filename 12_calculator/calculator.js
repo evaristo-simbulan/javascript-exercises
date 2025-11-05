e8c27a418d276523ffbecb00cce03970aac63793
evaristo-simbulan <219189860+evaristo-simbulan@users.noreply.github.com>
2025-11-05 18:21:23 +0800

    Add 12_calculator solution

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+  return input.reduce((sum, value) => sum += value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+  return input.reduce((sum, value) => sum *= value);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+  let result = 1;
+  for(let i = input; i > 1; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
